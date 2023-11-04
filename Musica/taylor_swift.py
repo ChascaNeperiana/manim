e3ada18cd6a3986be848866a02a9e8543e90f53a
Marco Montoya <marcoamontoyam@gmail.com>
2023-11-04 02:16:09 -0600

    refactoring: function to calculate vx and vy

--- Musica/taylor_swift.py
+++ Musica/taylor_swift.py
@@ -3,47 +3,75 @@ import random
 import math
 
 class Marjorie(Scene):
+    
+    # Each animation will be done during two beats
+    tempo = 5.0/4.0
+    limit_up = 2
+    limit_down = 2
+    movement_range = 2.0
+    
+    def move_rectangle(self, limit_left, limit_right, box_x, box_y):
+        # random movement directions
+        vx = random.uniform(-self.movement_range, self.movement_range)
+        vy = random.uniform(-self.movement_range, self.movement_range)
+        # normalize
+        l = math.sqrt(vx*vx + vy*vy)
+        vx/=l
+        vy/=l
+        
+        # Stay within limits
+        if (box_x + vx > limit_right):
+            vx = min(vx, -vx)
+            
+        elif (box_x + vx < limit_left):
+            vx = max(vx, -vx)
+            
+        if (box_y + vy > self.limit_up):
+            vy = min(vy, -vy)
+            
+        elif (box_y + vy < self.limit_down):
+            vy = max(vy, -vy)
+            
+        return [vx, vy]
+        
     def construct(self):
     
         # Rectangle that's the main character
         box = Rectangle (stroke_color = GREEN_C, fill_color=RED_B, fill_opacity=0.5, height=0.5, width=0.5)
         self.add(box)
-        # Normalized velocity vector to be updated every 
-        vx = random.uniform(-2,2)
-        vy = random.uniform(-2,2)
-        l = math.sqrt(vx*vx + vy*vy)
-        vx/=l
-        vy/=l
+
+        # Initial movement limits prior any lyrics
+        limit_left = -10
+        limit_right = 10
         
-        l0 = NumberLine(
-            x_range=[-10, 10, 2],
-            length=10,
+        line = NumberLine(
+            x_range=[-20, 20, 2],
+            length = 10,
             color=BLUE,
             include_numbers=False
         )
-        self.add(l0)
-        
-        # Each animation will be done during two beats
-        for i in range (6):
-            # The animation corresponds to half of Marjorie's bpm
-            self.play(box.animate.shift(RIGHT*vx + UP*vy), run_time=5.0/4.0)
-            vx = random.uniform(-2.0,2.0)
-            vy = random.uniform(-2.0,2.0)
-            l = math.sqrt(vx*vx + vy*vy)
-            vx/=l
-            vy/=l
+        self.add(line)
+       
+        # Song intro      
+        for i in range(6):
+            bx = box.get_center()[0]
+            by = box.get_center()[1]
+            mov = self.move_rectangle(limit_left, limit_right, bx, by)
+            print(bx, by)
+            # animate
+            self.play(box.animate.shift(RIGHT*mov[0] + UP*mov[1]), run_time=self.tempo)
         
         # Never be so kind you for get to be clever
-        for i in range (8):
-            t = Text("Never be so kind you forget to be clever", font_size=12)
-            t.set_y(2)
-            self.add(t)
-            self.play(box.animate.shift(RIGHT*vx + UP*vy), run_time=5.0/4.0)
-            vx = random.uniform(-2.0,2.0)
-            vy = random.uniform(-2.0,2.0)
-            l = math.sqrt(vx*vx + vy*vy)
-            vx/=l
-            vy/=l
-            # You forget to be clever when x > 2
-            if (box.get_center()[0] + vx > 2):
-                vx = min(vx, -vx)
+        t1 = Text("Never be so kind you forget to be clever", font_size=12)
+        t1.set_y(2)
+        self.add(t1)
+        limit_right = 3
+        for i in range(8):
+            bx = box.get_center()[0]
+            by = box.get_center()[1]
+            print(bx, by)
+            mov = self.move_rectangle(limit_left, limit_right, bx, by)
+            # animate
+            self.play(box.animate.shift(RIGHT*mov[0] + UP*mov[1]), run_time=self.tempo)
+            
+            
